c1b46a758ce35d014bd2770c328b12f0e5552edf
Najwa Khadijah <najwa.khadijah@mhs.politala.ac.id>
2025-10-29 09:18:22 +0800

    website agar dapat membaca semua excel

--- app/Imports/JadwalPerkuliahanImport.php
+++ app/Imports/JadwalPerkuliahanImport.php
@@ -4,23 +4,14 @@ namespace App\Imports;
 
 use App\Models\JadwalPerkuliahan;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class JadwalPerkuliahanImport implements ToModel, WithHeadingRow
+class JadwalPerkuliahanImport implements WithMultipleSheets
 {
-    public function model(array $row)
+    public function sheets(): array
     {
-        return new JadwalPerkuliahan([
-            'kode_matkul'       => $row['kode_mk_isian_lihat_daftar_mk_kurikulum'] ?? null,
-            'sistem_kuliah'     => $row['sistem_kuliah_isian_lihat_daftar_sistem_kuliah_gunakan_pemisah_koma_untuk_mendatakan_sebaran_sistem_kuliah'] ?? null,
-            'nama_kelas'        => $row['nama_kelas_isian'] ?? null,
-            'kelas_mahasiswa'   => $row['kelas_mahasiswa'] ?? null,
-            'sebaran_mahasiswa' => $row['sebaran_mahasiswa'] ?? null,
-            'hari'              => $row['hari'] ?? null,
-            'jam_mulai'         => $row['jam_mulai'] ?? null,
-            'jam_selesai'       => $row['jam_selesai'] ?? null,
-            'ruangan'           => $row['ruangan'] ?? null,
-            'daya_tampung'      => $row['daya_tampung'] ?? null,
-        ]);
+        return [
+            'Format Isian' => new JadwalPerkuliahanSheetImport(), // baca hanya sheet ini
+        ];
     }
 }
